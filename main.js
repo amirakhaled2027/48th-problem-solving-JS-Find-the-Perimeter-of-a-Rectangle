c9edbef5ab3c35c7052a65eaa07b6856cdaadfa5
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-07-15 23:46:43 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+//Create a function that takes length and width and finds the perimeter of a rectangle.
+
+// perimeter = (length + width) * 2;
+
+function recPerimeter(length, width) {
+    return (length + width) * 2
+}
+console.log(recPerimeter(6, 7));
+console.log(recPerimeter(20, 10));
+console.log(recPerimeter(2, 9));
